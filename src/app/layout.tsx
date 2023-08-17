29c896574c9d2d171f0ec951a717c92446d7b0a4
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2023-08-17 22:02:48 +0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Providers from "./providers"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "vadosjenkins",
+  title: "Vados Jenkins",
   icons:{
     icon: "/favicon.ico",
   }
